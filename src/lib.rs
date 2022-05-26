11ea342a7ac90964c7d7f44319f31ab806307810
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-05-26 22:59:28 +0200

    Use `as_mut_ptr` instead of `as_ptr` to mutate string
    Using `as_ptr` only gives read provenance since it goes
    through `&self`.

--- src/lib.rs
+++ src/lib.rs
@@ -211,7 +211,7 @@ impl<const N: usize> StrBuf<N> {
     ///
     ///To safely modify the slice, user must guarantee to write valid UTF-8
     pub unsafe fn as_mut_slice(&mut self) -> &mut [u8] {
-        slice::from_raw_parts_mut(self.as_ptr() as *mut u8, self.cursor as usize)
+        slice::from_raw_parts_mut(self.as_mut_ptr(), self.cursor as usize)
     }
 
     #[inline]
@@ -272,7 +272,7 @@ impl<const N: usize> StrBuf<N> {
     #[inline]
     ///Appends given string without any size checks
     pub unsafe fn push_str_unchecked(&mut self, text: &str) {
-        ptr::copy_nonoverlapping(text.as_ptr(), self.as_ptr().offset(self.cursor as isize) as *mut u8, text.len());
+        ptr::copy_nonoverlapping(text.as_ptr(), self.as_mut_ptr().offset(self.cursor as isize), text.len());
         self.set_len(self.cursor.saturating_add(text.len() as u8));
     }
 
